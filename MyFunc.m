f39f7ca9b163cc5c9c8d4f12d17bee04b1922bda
Davletshin Lenard <lenard_davletshin@mail.ru>
2020-10-14 14:07:04 +0500

    Initial commit

---
+++ MyFunc.m
@@ -0,0 +1,13 @@
+function S = MyFunc(x)
+%пока ничего не делает
+  T = 0;
+  n = 1;
+  temp = 0;
+  number = 10^(-5);
+  while abs(temp) > number
+    temp = ((-1)^n * (1+x)^(2*n))/n;
+    T = T + temp;
+    n = n + 1;
+  endwhile
+  disp(T);
+  end
---
+++ Untitled.m
@@ -0,0 +1,21 @@
+table = [];
+S = 0;
+temp = 1;
+n = 1;
+iter_1 = 1;
+iter_3 = 2;
+true;
+k = 1;
+while abs(temp) > 10^-5
+    temp = -temp * ((1 + x)^2/(n));
+    true = ((-1)^k)*((1+x)^(2*k)/k);
+    S = S + temp;
+    table = [table; temp true S];
+    n = n + 0.5;
+    k = k + 1;
+    %disp(temp);
+end
+
+my_table = array2table(table, 'VariableNames', {'Член Ряда','Правильный вариант','Сумма членов ряда'})
+writetable(my_table,'table_task2.csv');
+disp(S);
